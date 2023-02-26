4b23196432cc0e2311b7d9801d9d678c52ffd613
Laura Webber <laura@hightouch.io>
2023-02-26 18:39:17 +0000

    still debugging

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,24 +14,26 @@ from services.create_message import *
 from services.show_activity import *
 
 # import honeycomb otel modules
-#from opentelemetry import trace
-#from opentelemetry.instrumentation.flask import FlaskInstrumentor
-#from opentelemetry.instrumentation.requests import RequestsInstrumentor
-#from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-#from opentelemetry.sdk.trace import TracerProvider
-#from opentelemetry.sdk.trace.export import BatchSpanProcessor
-#from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
 # instantiate provider, processor, and tracer
-#provider = TracerProvider()
-#processor = BatchSpanProcessor(OTLPSpanExporter())
-#provider.add_span_processor(processor)
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 
 # instantiate instrumentors
-#FlaskInstrumentor().instrument_app(app)
-#RequestsInstrumentor().instrument()
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
 
 
 frontend = os.getenv('FRONTEND_URL')
